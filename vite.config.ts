936614cb9e8b8669a408871c3c669f0c29de5e89
PromiSe#### <vicimpa@ya.ru>
2023-08-03 18:29:28 +0700

    Fix architect

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import paths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: './src',
   base: './',
-  plugins: [svelte(), paths()],
+  build: {
+    outDir: '../dist'
+  },
+  plugins: [
+    svelte({ configFile: '../svelte.config.js' }),
+    paths()
+  ],
 });
